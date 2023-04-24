947979cdc607f2b463d42ff17f0dd7ecf628bb98
wisdm-nikhil <nikhil.mhaske@wisdmlabs.com>
2023-04-24 17:08:54 +0530

    Made some block specific changes

--- plugin.php
+++ plugin.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Content Tabs
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       Create Block to showcase content in Tabs
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Nikhil Mhaske
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       content-tabs
